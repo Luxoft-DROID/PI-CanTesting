55ee404223b62cf5a381f80af18af796c0229a57
root <root@thecatishere.thecatishere.org>
2017-12-13 03:21:19 +0200

    rawcanlessnodejs

--- index.js
+++ index.js
@@ -1,48 +1,37 @@
 const path = require('path')
 const express = require('express')
-const can = require('rawcan');
 const exphbs = require('express-handlebars')
 const port = 3000
 const app = express()
 const motorsId = 456;
 
-var socket = can.createSocket('can0');
-canlog = {}
+var canlog = {}
 var candumps = []
-canlog.candump = candumps;
+  canlog.candump = candumps;
 
-
+  function moveCode(direction) { 
+   if (direction == "up")
+	return "VICTORYGY";
+} 
 
   app.use('/', express.static(__dirname + '/'));
 
   app.post('/form', function (req, res) {
     console.log("recieved form");
     var cansignal = req.body.cansignal;
+	if (cansignal == null)
+		cansignal = moveCode(req.body.cancommand);
     var parsedCan = cansignal.split('#');
 
-    socket.send(parseInt(parsedCan[0], 16), parsedCan[1].substring(0, 8));
     console.log(parseInt(parsedCan[0], 16) + "#" + parsedCan[1].substring(0, 8));
   });
 
-  socket.on('message', (id, buffer) => {
-    var tempstring = 'received frame [' + id.toString(16) + '] ' + buffer.toString('hex');
-    console.log(tempstring);
-    var dump = {
-      "id" : id.toString(16),
-      "frame" : buffer.toString('hex')
-    }
-    canlog.candump.unshift(dump);
-  });
+ 
 
   app.post('/dump', function (req, res) {
     console.log(canlog);
     res.send(canlog);
-  });
-
-  app.post('/stop', function (req, res) {
-    console.log("stopped");
-    socket.send(parseInt(motorsId, 16), "00000000");
-    res.send("status : success");
+	canlog = {};
   });
 
   app.listen(port, (err) => {
