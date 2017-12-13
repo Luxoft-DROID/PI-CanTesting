e7cd2df1ef1a9010b50457f8101f46ac0581ccc4
Catishere <viktor.gyoshev@gmail.com>
2017-12-13 15:33:03 +0200

    update

--- index.js
+++ index.js
@@ -7,17 +7,24 @@ const app = express()
 const motorsId = 456;
 
 var socket = can.createSocket('can0');
-canlog = {}
-var candumps = []
-canlog.candump = candumps;
+
+  function moveCode(direction) {
+     if (direction == "up")
+  	  return "VICTORYGY";
+  }
 
 
+  var canlog = {}
+  var candumps = []
+  canlog.candump = candumps;
 
   app.use('/', express.static(__dirname + '/'));
 
   app.post('/form', function (req, res) {
     console.log("recieved form");
     var cansignal = req.body.cansignal;
+  	if (cansignal == null)
+  		cansignal = moveCode(req.body.cancommand);
     var parsedCan = cansignal.split('#');
 
     socket.send(parseInt(parsedCan[0], 16), parsedCan[1].substring(0, 8));
@@ -35,14 +42,8 @@ canlog.candump = candumps;
   });
 
   app.post('/dump', function (req, res) {
-    console.log(canlog);
     res.send(canlog);
-  });
-
-  app.post('/stop', function (req, res) {
-    console.log("stopped");
-    socket.send(parseInt(motorsId, 16), "00000000");
-    res.send("status : success");
+    canlog.candump = [];
   });
 
   app.listen(port, (err) => {
